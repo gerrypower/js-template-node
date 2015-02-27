3824644fe2501f179f830e9240e0682d7f3deeb6
Gerry Power <gerry@thepowerhouse.com>
2015-02-27 07:45:16 -0500

    Add lib and conf directories

--- gulp/config.js
+++ gulp/config.js
@@ -4,14 +4,16 @@ var moduleSrc = ['module-name.js'];
 var specDir = 'spec';
 var specs = specDir + '/**/*spec.js';
 var gulpTasks = ['gulpfile.js', 'gulp/**/*.js'];
-var allSrc = moduleSrc.concat(specs).concat(gulpTasks);
+var src = ['lib/**/*.js', 'conf/*.js'];
+var conf = ['conf/*.json'];
+var allSrc = moduleSrc.concat(specs).concat(gulpTasks).concat(src);
 
 module.exports = {
   lint: {
     src: allSrc
   },
   watch: {
-    test: allSrc,
+    test: allSrc.concat(conf),
     lint: allSrc
   }
 };

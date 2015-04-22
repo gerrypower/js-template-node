a006ae0c4a3e9f10bfaf7e7a610bd1f44622a79d
Gerry Power <gerry@thepowerhouse.com>
2015-04-22 10:35:25 -0400

    Remove extra spaces

--- gulp/tasks/development/watch.js
+++ gulp/tasks/development/watch.js
@@ -4,6 +4,6 @@ var gulp = require('gulp');
 var config = require('../../config');
 
 gulp.task('watch', function() {
-  gulp.watch(config.watch.lint,  ['lint']);
-  gulp.watch(config.watch.test,  ['test']);
+  gulp.watch(config.watch.lint, ['lint']);
+  gulp.watch(config.watch.test, ['test']);
 });

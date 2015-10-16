90eb61509b18779c45e45a0b3bfe809e1b8c36ec
Gerry Power <gerry@thepowerhouse.com>
2015-10-16 12:19:03 -0400

    Drop gulp-eslint and use custom lint task

--- gulp/tasks/development/lint.js
+++ gulp/tasks/development/lint.js
@@ -1,12 +1,30 @@
 'use strict';
 
+var eslint = require('eslint/lib/cli');
 var gulp = require('gulp');
-var eslint = require('gulp-eslint');
+var globby = require('globby');
 var config = require('../../config');
 
-gulp.task('lint', function () {
-  return gulp.src(config.lint.src)
-      .pipe(eslint())
-      .pipe(eslint.format())
-      .pipe(eslint.failOnError());
+// run eslint without an additional plugin
+gulp.task('lint', function(done) {
+
+  // patterns with the same form as gulp.src(patterns)
+  var patterns = config.lint.src;
+
+  globby(patterns).then(function(paths) {
+    var code = eslint.execute(paths.join(' '));
+
+    if (err) {
+      done(err);
+      return;
+    }
+
+    // additional CLI options can be added here
+    if (code) {
+      // eslint output already written, wrap up with a short message
+      done(new gutil.PluginError('lint', new Error('ESLint error')));
+      return;
+    }
+    done();
+  });
 });
